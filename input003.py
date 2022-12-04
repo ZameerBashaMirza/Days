c5ead9b80f6fddc64445ae376425412bde3c894c
Zameer Basha Mirza <zameerbashamirza@gmail.com>
2022-12-04 16:18:44 +0530

    003

---
+++ input003.py
@@ -0,0 +1,18 @@
+print("Tell me your name: ")
+print("Tell me your last name: ")
+
+
+input("Tell me your name: ")
+input("Tell me your last name: ")
+
+print(input("Tell me your name: "))
+
+print("Your name is " + input("Tell me your name: "))
+
+print("Your name is " + (input("Tell me your name: "))+" " +(input("Tell me your last name: ")))
+
+print(input("What are you learning today ? "))
+
+print(input("Where are you from?"))
+
+print(input("What is your first name? ")+" "+input("What is your Surname?"))
