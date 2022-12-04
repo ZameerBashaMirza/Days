e5941f5ebf2f4a6bab72cdbc745402a08e745a5c
Zameer Basha Mirza <zameerbashamirza@gmail.com>
2022-12-04 17:03:06 +0530

    Day1 Solo Challenge

---
+++ Day1_Python_Challenge.py
@@ -0,0 +1,10 @@
+
+print("It's Awesome  \""
+      +input("what is the name of the creature we loves in our childhood ?")+" "
+      +input(" At what age you like it?")+"\"")
+
+print("It's Awesome  \n\""
+      +input("what is the name of the creature we loves in our childhood ?")+" "
+      +input(" At what age you like it?")+"\"")
+
+
